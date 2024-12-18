6278ae3547e3266704501da6deaef832ea1a549a
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-12-18 23:09:24 +0530

    noting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   integrations: [tailwind(), react()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function Test() {
-  return <div>This is test page</div>;
+  const [count, setcount] = useState<number>(0);
+  return (
+    <div className="flex justify-center h-full items-center">
+      <div className=" flex flex-col gap-4 items-center">
+        <div className=" text-xl font-bold">{count}</div>
+        <button
+          onClick={() => setcount(count + 1)}
+          className="shadow border py-2  px-8 rounded-xl"
+        >
+          Click
+        </button>
+      </div>
+    </div>
+  );
 }
